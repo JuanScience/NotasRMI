ad52103f3983a9d0aa16ba97b66c6f2f61d31327
Juan <123@DESKTOP-8MG0RMP>
2022-09-05 12:22:33 -0500

    Version 1.0

--- NotasRMI/src/Interface/NotasInterface.java
+++ NotasRMI/src/Interface/NotasInterface.java
@@ -4,11 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Map;
 
-/**
- * @authors
- * Ormolgud Gonzalez Cardona
- * Juan Carlos Salazar Muñoz
- */
 public interface NotasInterface extends Remote{
     public Map generate(int s, int n) throws RemoteException;
 }
